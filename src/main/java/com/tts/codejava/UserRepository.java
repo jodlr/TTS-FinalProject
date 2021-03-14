58e37cb09cb2bd75c1b351d238e22c0130f8f2d5
jodlr <johndelarosa_netplus@yahoo.com>
2021-03-13 19:38:40 -0600

    Comopleted jUnitTest

---
+++ src/main/java/com/tts/codejava/UserRepository.java
@@ -0,0 +1,7 @@
+package com.tts.codejava;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
---
+++ src/test/java/com/tts/codejava/UserRepositorytests.java
@@ -0,0 +1,43 @@
+package com.tts.codejava;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositorytests {
+	
+	@Autowired
+    private TestEntityManager entityManager;
+     
+    @Autowired
+    private UserRepository repo;
+     
+    // test methods go below
+    
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setEmail("v.1jodlr@gmail.com");
+        user.setPassword("abc123");
+        user.setFirstName("John");
+        user.setLastName("Delarosa");
+         
+        User savedUser = repo.save(user);
+         
+        User existUser = entityManager.find(User.class, savedUser.getId());
+         
+        assertThat(user.getEmail()).isEqualTo(existUser.getEmail());
+         
+    }
+    
+
+}
