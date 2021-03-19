abb4dce18ecf17d2a5733b12f2c9aaaaac084fb4
jodlr <johndelarosa_netplus@yahoo.com>
2021-03-18 23:24:58 -0500

    Thu-Mar-18:11:243 pm

--- src/main/java/com/tts/usrreg/DBPassGen.java
+++ src/main/java/com/tts/usrreg/DBPassGen.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class DBPassGen {
  public static void main(String[] args ) {	
 	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-	String txtPassword = "ChangeMe!";
+	String txtPassword = "password123";
     String GeneratedPwd =encoder.encode(txtPassword);
     
     System.out.println(GeneratedPwd);
